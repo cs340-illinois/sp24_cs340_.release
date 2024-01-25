e3ca11bd9619da5602cd038028ca5b4aec740b90
Luther Tychonievich <tychonievich@gmail.com>
2024-01-25 17:46:55 -0600

    add mp2pp

---
+++ mp2pp/png-dimensions.c
@@ -0,0 +1,11 @@
+#include <sys/types.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <fcntl.h>
+
+int png_dimensions(const char *png_filename, uint32_t *width, uint32_t *height) {
+	return 1;
+}
